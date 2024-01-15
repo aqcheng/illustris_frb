b9215bb2bd50927e4264ce308d17db251142ec99
April Cheng <aqc@mit.edu>
2024-01-15 03:19:25 +0000

    code now works with ray tracing in negative directions and arbitrary origins

--- illustris_frb/__init__.py
+++ illustris_frb/__init__.py
@@ -10,11 +10,33 @@ import astropy.constants as const
 
 from scipy.interpolate import interp1d
 
-from .utils import signed_range, get_box_crossings
+from .utils import get_box_crossings
 
 class simulation:
+    """
+    An object for the attributes of an IllustrisTNG simulation, including the 
+    simulation boxsize, paths to the electron density maps and simulation 
+    files, and cosmology.
+    """
     
-    def __init__(self, name, binsize, sim_dir=None, map_dir=None):
+    def __init__(self, name, binsize=500, sim_dir=None, map_dir=None):
+        """
+        Parameters
+        ----------
+        name: str
+            The name of the simulation, e.g. 'TNG205n2500'. Used for setting
+            the default simulation and map directory paths.
+        binsize: int, optional
+            The size of the bin for the electron density map, in ckpc/h. Must
+            divide the simulation boxsize. Default: 500
+        sim_dir: str, optional
+            Directory to simulation snapshot files. Defaults to the path on
+            Illustris JupyterHub.
+        map_dir: str, optional
+            Directory to electron density map. Defaults to './n_e_maps/{name}'
+        """
+        
+        
         self.name = name
         
         if sim_dir is None:
@@ -44,34 +66,88 @@ class simulation:
         return os.path.join(self.sim_dir, f'snapdir_{snap:03}')
 
     def get_chunk_path(self, snap, chunk=0):
-        return os.path.join(self.get_snapdir_path(snap), f'snap_{snap:03}.{chunk}.hdf5')
+        return os.path.join(self.get_snapdir_path(snap), 
+                            f'snap_{snap:03}.{chunk}.hdf5')
     
     def get_map_path(self, snap):
         return os.path.join(self.map_dir, f'{snap}.npy')
     
     def get_snapshot_dists(self):
+        """
+        Retrieve and compute the redshifts and corresponding comoving distances
+        of each simulation snapshot.
+        
+        Returns
+        -------
+        snap_xs: (100,) array
+            The comoving distances corresponding to the redshift of each
+            snapshot. snap_xs[i] corresponds to snapshot i.
+        snap_zs: (100,) array
+            The redshift of each snapshot. snap_zs[i] corresponds to snapshot
+            i.
+        """
         snap_zs = []
         for snap in range(100):
             with h5py.File(self.get_chunk_path(snap)) as f:
                 snap_zs.append(f['Header'].attrs['Redshift'])
         snap_zs = np.array(snap_zs) * cu.redshift
-        snap_xs = self.cosmo.comoving_distance(snap_zs).to(u.kpc / cu.littleh, self.h_equiv).value
+        snap_xs = self.cosmo.comoving_distance(snap_zs).to(u.kpc / cu.littleh, 
+                                                           self.h_equiv).value
         return snap_xs, snap_zs
     
 class frb_simulation(simulation):
+    """
+    A class for ray tracing to FRBs in IllustrisTNG from a specified origin. 
+    Inherits from the simulation class. 
+    """
     
-    def __init__(self, name, binsize, origin=np.array([0,0,0]), sim_dir=None, map_dir=None, max_z=2):
+    def __init__(
+        self, 
+        name, 
+        binsize, 
+        origin=np.array([0,0,0]), 
+        sim_dir=None, 
+        map_dir=None, 
+        max_z=2
+    ):
+        """
+        Parameters
+        ----------
+        origin: (3,) array
+            The coordinates of the observer in the simulation.
+        max_z: float, optional
+            The maximum redshift out to which FRBs will be simulated. Used
+            only for creating the z(x) interpolant. Default: 2
+        """
+        
         simulation.__init__(self, name, binsize, sim_dir, map_dir)
         self.origin=origin
         
         self.snap_xs, self.snap_zs = self.get_snapshot_dists()
         
         # create interpolant for determining redshifts from comoving distances
-        redshift_vals = np.linspace(0,2,100000)
-        dist_vals = self.cosmo.comoving_distance(np.linspace(0,2,100000)).to(u.kpc/cu.littleh, self.h_equiv).value
+        redshift_vals = np.linspace(0,max_z,100000)
+        dist_vals = self.cosmo.comoving_distance(redshift_vals).to(
+            u.kpc/cu.littleh, self.h_equiv).value
         self.z_from_dist = interp1d(dist_vals, redshift_vals)
     
     def ray_trace(self, dest):
+        """
+        Ray traces from observer to FRB through the electron map to return 
+        $n_e(x)$.
+        
+        Returns
+        -------
+        x_edge_dists: (N+1,) array
+            An array of the distances from the observer to all bin crossings
+            plus end points, in ckpc/h.
+        xs: (N,) array
+            An array of the distances from the observer to the midpoints of the
+            ray in each bin, in ckpc/h.
+        nes: (N,) array
+            An array containing the electron number density $n_e$ in each bin,
+            in (ckpc/h)^{-3}.
+        """
     
         x_edge_dists = [0] #edge distances: for riemann integration
         xs = [] #bin midpoints, for calculating redshift, n_e(x), etc.
@@ -79,7 +155,8 @@ class frb_simulation(simulation):
 
         Vcell = self.binsize**3
 
-        sim_box_edges, sim_box_edge_dists, sim_box_gridcoords = get_box_crossings(dest, self.origin, self.boxsize)
+        sim_box_edges, sim_box_edge_dists, sim_box_coords = get_box_crossings(
+            dest, self.origin, self.boxsize)
 
         current_pos = self.origin
         traveled_dist = 0
@@ -89,20 +166,29 @@ class frb_simulation(simulation):
 
         for box_idx in range(len(sim_box_edges)):
 
-            offset = self.boxsize * sim_box_gridcoords[box_idx] #offset sim box coordinates
+            offset = self.boxsize * sim_box_coords[box_idx] #offset sim box coordinates
             next_pos = sim_box_edges[box_idx]
 
-            bin_edges, bin_edge_dists, bin_gridcoords = get_box_crossings(next_pos-offset, current_pos-offset, self.binsize)
-            # print(box_idx, bin_gridcoords[0], bin_gridcoords[-1])
+            bin_edges, bin_edge_dists, bin_coords = get_box_crossings(
+                next_pos-offset, current_pos-offset, self.binsize)
+
+            bin_coords = np.mod(bin_coords, self.n_bins)
+            bin_indices = bin_coords[:, 0] * self.n_bins**2 + \
+                          bin_coords[:, 1] * self.n_bins + \
+                          bin_coords[:, 2]
+            
             bin_edge_dists += traveled_dist
-            bin_mid_dists = np.convolve( np.insert(bin_edge_dists, 0, traveled_dist), [0.5, 0.5], 'valid' )
-
-            bin_indices = bin_gridcoords[:, 0] * self.n_bins**2 + bin_gridcoords[:, 1] * self.n_bins + bin_gridcoords[:, 2]
-            bin_snaps = np.argmin(np.abs(np.repeat(np.atleast_2d(bin_mid_dists), 100, axis=0).T - self.snap_xs), axis=1) #find closest snapshot for each bin
-
+            bin_mid_dists = np.convolve(
+                np.insert(bin_edge_dists, 0, traveled_dist), [0.5, 0.5], 
+                'valid')
+            bin_snaps = np.argmin(np.abs(np.repeat(
+                np.atleast_2d(bin_mid_dists), 100, axis=0
+            ).T - self.snap_xs), axis=1) #find closest snapshot for each bin
+            
             for bidx in range(len(bin_edges)): #loop through bins
                 snap = bin_snaps[bidx]
-                if snap != open_snap: #if corresponding snapshot is not open, open it
+                #if corresponding snapshot is not open, open it
+                if snap != open_snap: 
                     open_snap = snap
                     open_map = np.load(self.get_map_path(snap))
 
@@ -112,25 +198,52 @@ class frb_simulation(simulation):
 
             current_pos = next_pos
             traveled_dist = sim_box_edge_dists[box_idx]
+            
 
         return np.array(x_edge_dists), np.array(xs), np.array(nes) 
-        #returns n_e(x) where x is in ckpc/h, n_e is in (ckpc/h)**3, in order of increasing comoving distance (x)
-        #x_edge_dists is 1 longer than nes
         
     def compute_DM(self, x_edge_dists, xs, nes, cumulative=False):
+        """
+        Computes the DM given the output of ray_trace.
+        """
     
         nes = (nes * (u.kpc/cu.littleh)**(-3)).to(u.cm**(-3), self.h_equiv)
         zs = self.z_from_dist(xs)
 
         y = nes * (1 + zs) 
-        dx = np.diff( (x_edge_dists * u.kpc/cu.littleh).to(u.pc, self.h_equiv) )
+        dx = np.diff((x_edge_dists * u.kpc/cu.littleh).to(u.pc, self.h_equiv))
 
         if cumulative:
-            return np.flip(x_edge_dists[-1]-xs)*u.kpc/cu.littleh, np.cumsum(np.flip(y * dx)) #integrate from FRB to observer
+            return np.flip(x_edge_dists[-1]-xs)*u.kpc/cu.littleh, \
+                   np.cumsum(np.flip(y * dx)) #integrate from FRB to observer
         else:
             return np.sum(y * dx)
     
     def get_frb_DM(self, dest, cumulative=False):
+        """
+        Given an FRB location, ray traces through binned simulation snapshots 
+        to compute the DM via Riemann integration.
+        
+        If cumulative, then `dist_from_FRB` and `cumulative_DM` is returned,
+        which can be plotted to track the accumulated DM as the ray travels
+        from FRB to observer.
+        
+        If not cumulative, then the total DM is returned.
+        
+        Parameters
+        ----------
+        dest: (3,) array
+            The coordinates of the FRB in the simulation.
+        cumulative: bool, optional
+            Whether or not to return the cumulative or total DM.
+        
+        Returns
+        -------
+        dist_from_FRB: (N,) array
+            Bin midpoint distances from the FRB with astropy units.
+        cumulative_DM: (N,) array
+            The accumulated DM along the ray from the FRB with astropy units.
+        """
         
         x_edge_dists, xs, nes = self.ray_trace(dest)
         return self.compute_DM(x_edge_dists, xs, nes, cumulative)
--- illustris_frb/utils.py
+++ illustris_frb/utils.py
@@ -1,13 +1,33 @@
 import numpy as np
 from numpy.linalg import norm
+from math import floor, ceil
 
-def signed_range(q):
-    if q >= 0:
-        return np.arange(q)
+def integers_between(a, b):
+    """
+    Computes all integers between `a` and `b`.
+    """
+    
+    if a < b:
+        return np.arange(floor(a)+1, floor(b)+1)
     else:
-        return np.arange(0, q, -1)
+        return np.arange(ceil(a)-1, ceil(b)-1, -1)
+
 
 def get_box_crossings(dest, origin, boxsize): 
+    """
+    Given a ray traveling between two points, finds the intersection points of
+    a 3D grid of integer multiples of boxsize.
+
+    Returns
+    -------
+    all_edges: (N,3) array
+        The list of all crossed bin edges from origin to destination, and the
+        destination coordinates.
+    all_edge_dists: (N,) array
+        The distances from origin to each point in all_edges.
+    all_box_gridcoords: (N,3) array
+        The list of the grid coordinates of the bins that the ray travels through.
+    """
     
     edges_list = [dest]
     edge_dists_list = [norm(np.atleast_2d(dest-origin), axis=1)]
@@ -15,20 +35,21 @@ def get_box_crossings(dest, origin, boxsize):
     for i in range(3): #loop over dimensions, get all edge crossings in each dimension
         x_f = dest[i]
         x_0 = origin[i]
-        if (x_f//boxsize) == (x_0//boxsize): #no edge crossings
+        
+        x_crossings = boxsize*integers_between(x_0/boxsize, x_f/boxsize)
+        if len(x_crossings)==0:
             continue
-            
-        unit_vec_i = (dest - origin)/abs(x_f - x_0)
-        edge_1 = origin + unit_vec_i * (boxsize - x_0%boxsize)
-        
-        q = abs(x_f - edge_1[i]) // boxsize + 1 # no. of total edge crossings in the x-direction
-        
-        edges = edge_1 + np.atleast_2d(unit_vec_i) * boxsize * np.atleast_2d(signed_range(q)).T
+        m = (dest - origin)/(x_f - x_0)
+        # y = y_0 + m(x-x_0)
+        edges = origin + np.atleast_2d(m) * np.atleast_2d(x_crossings - x_0).T
+
         edges_list.append(edges)
         edge_dists_list.append(norm(edges - origin, axis=1))
     
     all_edge_dists, index_arr = np.unique(np.concatenate(edge_dists_list), return_index=True)
     all_edges = np.vstack(edges_list)[index_arr]
-    all_box_gridcoords = (np.vstack((np.atleast_2d(origin), all_edges[:-1])) / boxsize).astype(int)
     
+    all_midpoints = (np.vstack((np.atleast_2d(origin), all_edges[:-1])) + all_edges)/2
+    all_box_gridcoords = (all_midpoints // boxsize).astype(int)
+
     return all_edges, all_edge_dists, all_box_gridcoords
