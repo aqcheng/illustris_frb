8b665db5e81e78b880d617208e99891f394db01e
April Q Cheng <aqc@mit.edu>
2024-08-15 13:37:47 -0400

    added feature to automatically aggregate results from scratch directory and delete scratch directory

--- scripts/compute_DM_partition.py
+++ scripts/compute_DM_partition.py
@@ -1,16 +1,12 @@
 import sys
 sys.path.append('/home/submit/aqc/frb_project')
-
+import os
 import numpy as np
-from illustris_frb import exp_simulation, region
-
+from illustris_frb import exp_simulation
 
 ## --- INPUTS ---
 
 origin = 500 * np.array([50, 70, 23])
-reg = region((1.18, -np.pi/2), 1.11, 1.88)
-region_name = 'A1'
-suffix = 'res0008'
 nproc = 32
 
 ## --- COMMAND LINE INPUTS ##
@@ -19,9 +15,18 @@ import argparse
 argp = argparse.ArgumentParser()
 argp.add_argument("-N", type=int, required=True, help="Total # of partitions. Should divide the # of rows on a side.")
 argp.add_argument("-n", type=int, required=True, help="Partition number; must be between 0 and N-1.")
+argp.add_argument("--reg", type=str, required=True, help="The name of the region")
 args = argp.parse_args()
 
 ## --- END OF INPUTS ---
 
-sim = exp_simulation(origin, reg, region_name=region_name, suffix=suffix)
-sim.compute_DM_grid_partition(N=args.N, n=args.n, nproc=nproc)
+sim = exp_simulation(origin, reg_name=args.reg)
+if not os.path.isfile(os.path.join(sim.scratch_path, f'cumulative_DM_{args.n:03}.npy')):
+    sim.compute_DM_grid_partition(N=args.N, n=args.n, nproc=nproc)
+
+if len(os.listdir(sim.scratch_path)) > args.N:
+    if os.path.exists(sim.results_path):
+        os.remove(sim.results_path)
+    sim.DM_grid()
+    from shutil import rmtree
+    rmtree(sim.scratch_path)
